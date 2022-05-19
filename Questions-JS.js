96447ca9d59a34e68182bcb4f937a457d46a60aa
mitchg1983 <mitchg83@Geoffreys-MacBook-Pro.local>
2022-05-19 10:35:28 -0400

    Add exercvise code.

--- Questions-JS.js
+++ Questions-JS.js
@@ -1,14 +1,34 @@
-const x = 6
+const x = 6;
+let aA = 4;
+let bB = 7;
 
 // 7. Write a function that takes 2 numbers as arguments and returns the sum of both numbers and the variable "x" using without using arrow functions.
 
+function questionSeven(num1, num2, numX) {
+  let preSum = num1 + num2;
+  let finalSum = preSum + numX;
+  console.log("The finalSum is...", finalSum);
+  return finalSum;
+}
+questionSeven(aA, bB, x);
+
 // 8. Write a function that takes 2 numbers as arguments and returns the sum of both numbers and the variable "x", using arrow functions.
+
+const questionEight = (num1, num2, numX) => {
+  return num1 + num2 + numX;
+};
+
+console.log("Answer to questionEight is...", questionEight(aA, bB, x));
 
 // 9. Write a function that returns another function. (use arrow functions please)
 
+const addThis = (numAdd) => {
+  return questionEight(aA + numAdd, bB + numAdd, x + numAdd);
+};
+
+console.log("The answer to questionNine is...", addThis(4));
 
 // 10. given the following code:
-
 
 const getFunction = () => {
   const y = 5;
@@ -18,21 +38,18 @@ const getFunction = () => {
   return insideFunc;
 };
 
-console.log(getFunction()(2))
+console.log(getFunction()(2));
 
 // Explain why the function that returns from getFunction still has access to "y" if "y" is not a global variable.
-
-
 
 // 11. write a function that takes "couldThrowError()" as a callback argument.
 // within that function call "couldThrowError" and console log its result.
 // Make sure to handle errors that could be thrown by "couldThrowError()"
 
 const couldThrowError = () => {
-  
-  if(Math.ceil(Math.random() * 2) < 2){
+  if (Math.ceil(Math.random() * 2) < 2) {
     throw new Error("Error was thrown");
   }
-  
-  return 'success'
-}
+
+  return "success";
+};
